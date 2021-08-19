c8dc36399cf327e444301ee1dab446fbe01c4ab4
Rusty3141 <89071215+Rusty3141@users.noreply.github.com>
2021-08-19 12:10:39 +0100

    Improve frequency plots

--- tools/rune_frequency_plotter.py
+++ tools/rune_frequency_plotter.py
@@ -1,12 +1,31 @@
 import os
-import matplotlib
+import sys
+import getopt
+
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+from matplotlib.ticker import AutoMinorLocator
 
 
-def main():
-    runebet = "ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ"
+def analyse_gutenberg_runic(path, runebet):
+    with open(path, encoding="utf8") as book:
+        stringData = "".join(
+            [x for x in book.readlines() if not(x.startswith('#'))])
+        frequencyData = {}
+        analyse(runebet, frequencyData, stringData)
+        print(frequencyData)
+
+
+def plot_frequencies(plt, dict, **extraArgs):
+    plt.plot(sorted(dict.values(), reverse=True), **extraArgs)
+
+
+def scatter_frequencies(plt, dict, **extraArgs):
+    plt.scatter(sorted(dict.keys(), key=lambda x: dict[x], reverse=True), sorted(
+        dict.values(), reverse=True), **extraArgs)
+
+
+def write_section_plots(runebet):
     partsDirectory = os.path.join(os.path.dirname(__file__), "../raw-data/")
 
     plt.rcParams["figure.figsize"] = [16, 9]
@@ -17,61 +36,121 @@ def main():
     plt.rcParams["mathtext.rm"] = "Computer Modern Serif"
     plt.rcParams["mathtext.it"] = "Computer Modern Serif Italic"
     plt.rcParams['axes.unicode_minus'] = False
+    plt.rcParams['axes.xmargin'] = 0.02
 
     fm.fontManager.ttflist.append(
         fm.FontEntry(fname="C:/Windows/Fonts/cmunrm.ttf", name="Computer Modern Serif"))
     fm.fontManager.ttflist.append(
         fm.FontEntry(fname="C:/Windows/Fonts/cmunti.ttf", name="Computer Modern Serif Italic"))
+
     # This font must support runic characters.
     fm.fontManager.ttflist.append(fm.FontEntry(
         fname="C:/Windows/Fonts/seguihis.ttf", name="Segoe UI Historic"))
 
-    for part in os.listdir(partsDirectory):
+    parts = next(os.walk(partsDirectory))[1]
+    for part in parts:
         for i, sectionPath in enumerate(os.listdir(os.path.join(partsDirectory, part))):
             with open(os.path.join(partsDirectory, part, sectionPath), encoding='utf8') as sectionFile:
                 stringData = "".join(sectionFile.readlines())
-                frequencyData = {}
-                for rune in runebet:
-                    frequencyData[rune] = 0
-                for char in stringData:
-                    if char in frequencyData:
-                        frequencyData[char] += 1
+                actualFrequencies = {}
+                analyse(runebet, actualFrequencies, stringData)
 
             # Force integral values on y-axis ticks.
             plt.figure().gca().yaxis.get_major_locator().set_params(integer=True)
 
-            plt.gca().set_axisbelow(True)
-            plt.gca().yaxis.set_minor_locator(MultipleLocator(1))
-            plt.grid(True, which="both", alpha=0.4)
+            fig = plt.figure()
 
-            plt.margins(x=0.02)
+            actualAxes = fig.add_subplot(111, label="1")
+            idealAxes = fig.add_subplot(
+                111, label="2", frame_on=False, sharey=actualAxes)
 
-            plt.plot(sorted(frequencyData.values(), reverse=True))
-            plt.scatter(sorted(frequencyData.keys(), key=lambda x: frequencyData[x], reverse=True),
-                        sorted(frequencyData.values(), reverse=True))
+            idealAxes.set_axisbelow(True)
+            actualAxes.set_axisbelow(True)
 
-            flatY = sum(frequencyData.values()) / len(frequencyData.values())
-            plt.axhline(y=flatY, linestyle="--",
-                        linewidth=2, color="orange", label=f"Uniformly distributed text, $y={round(flatY, 1)}$")
-            # plt.annotate(, xy=(
-            # 0, flatY), xytext=(len(frequencyData.values()) - 3, flatY + 0.5))
+            actualAxes.xaxis.tick_bottom()
+            actualAxes.tick_params(axis="x")
+            actualAxes.set_xticklabels(sorted(actualFrequencies.keys(
+            ), key=lambda x: actualFrequencies[x]), fontfamily="Segoe UI Historic")
+
+            actualAxes.yaxis.set_minor_locator(AutoMinorLocator())
+            actualAxes.grid(True, which="both", alpha=0.4)
+
+            plot_frequencies(actualAxes, actualFrequencies,
+                             linewidth=2, label="Frequencies for this section", zorder=1)
+            scatter_frequencies(
+                actualAxes, actualFrequencies, linewidth=2, zorder=1)
+
+            flatY = sum(actualFrequencies.values()) / \
+                len(actualFrequencies.values())
+            idealAxes.axhline(y=flatY, linestyle="--", linewidth=2, color="orange",
+                              label=f"Uniformly distributed text ($y={round(flatY, 1)})$", alpha=0.5)
+
+            naturalFrequencies = {'ᚠ': 4812, 'ᚢ': 7765, 'ᚦ': 7725, 'ᚩ': 14961, 'ᚱ': 11598, 'ᚳ': 6525, 'ᚷ': 2930, 'ᚹ': 5539, 'ᚻ': 5795, 'ᚾ': 12774, 'ᛁ': 12473, 'ᛄ': 0, 'ᛇ': 31, 'ᛈ': 3774,
+                                  'ᛉ': 231, 'ᛋ': 13515, 'ᛏ': 13891, 'ᛒ': 2901, 'ᛖ': 25366, 'ᛗ': 5535, 'ᛚ': 8730, 'ᛝ': 2388, 'ᛟ': 17, 'ᛞ': 10340, 'ᚪ': 15869, 'ᚫ': 10, 'ᚣ': 3919, 'ᛡ': 657, 'ᛠ': 1829}
+
+            # Normalise by scaling the Gutenberg data to fit the section counts.
+            normalisationConstant = sum(
+                naturalFrequencies.values()) / sum(actualFrequencies.values())
+            naturalFrequencies = {
+                k: v / normalisationConstant for k, v in naturalFrequencies.items()}
+
+            plot_frequencies(idealAxes, naturalFrequencies, label="Natural frequencies for runic plaintext",
+                             linestyle="--", linewidth=2, color="green")
+            scatter_frequencies(idealAxes, naturalFrequencies, color="green")
+            idealAxes.xaxis.tick_bottom()
+            idealAxes.tick_params(axis="x", colors="green", labelsize=12)
+
+            # Shift ticks downwards
+            idealAxes.tick_params(axis="x", direction="in", pad=30)
 
             plt.xticks(fontname="Segoe UI Historic")
+
             plt.gca().set_title(
                 f"Rune frequencies in section {part}_{sectionPath.split('.')[0].zfill(2)}.txt, sorted from most to least common", pad=22)
-            plt.ylabel(
-                "Frequency")
+            plt.xlabel("Rune character")
+            plt.ylabel("Frequency")
             plt.tight_layout()
 
-            plt.legend()
+            plt.figlegend(bbox_to_anchor=[0.98, 0.9], loc="upper right")
 
             plt.savefig(os.path.join(os.path.dirname(
                 __file__), f"../docs/assets/images/LP/frequency-data/{part}/relative-rune-frequencies-{sectionPath.split('.')[0].zfill(2)}.png"))
-            plt.clf()
-            plt.cla()
-            print(
-                f"{i + 1}/{sum(len(os.listdir(os.path.join(partsDirectory, part))) for part in os.listdir(partsDirectory))} plots made and saved.", flush=True)
+            print(f"{i + 1}/{sum(len(os.listdir(os.path.join(partsDirectory, part))) for part in parts)} plots made and saved.", flush=True)
+            plt.close()
+
+
+def analyse(runebet, frequencyData, stringData):
+    for rune in runebet:
+        frequencyData[rune] = 0
+    for char in stringData:
+        if char in frequencyData:
+            frequencyData[char] += 1
+
+
+def print_help():
+    print("Usage:")
+    print(r"    rune_frequency_plotter.py (-h|--help|-g <gutenberg runic file path>|--gutenberg <gutenberg runic file path>)?")
+    print("    Entering gutenberg mode and passing a file path analyses the text to find a \"natural\" distribution.")
+
+
+def main(argv):
+    opts = []
+    try:
+        opts, args = getopt.getopt(argv, "hg:", ["help", "gutenberg="])
+    except getopt.GetoptError:
+        print_help()
+        sys.exit(2)
+
+    runebet = "ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ"
+    for opt, arg in opts:
+        if opt in ["-h", "--help"]:
+            print_help()
+            return
+        elif opt in ["-g", "--gutenberg"]:
+            analyse_gutenberg_runic(arg, runebet)
+            return
+    write_section_plots(runebet)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
