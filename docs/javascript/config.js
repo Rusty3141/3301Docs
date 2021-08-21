e38cdad9c8f868deebcc262f377cb6d56c6c3914
Rusty3141 <89071215+Rusty3141@users.noreply.github.com>
2021-08-21 12:21:22 +0100

    Fix lightGallery not loading if clicking immediately after page load

--- docs/javascript/config.js
+++ docs/javascript/config.js
@@ -16,4 +16,9 @@ document$.subscribe(() => {
   const nodes = document.querySelectorAll('.timeago');
   const locale = nodes[0].getAttribute('locale');
   timeago.render(nodes, locale);
+
+  var elements = document.getElementsByClassName("md-content");
+  for (var i = 0; i < elements.length; i++) {
+    lightGallery(elements[i], { selector: '.lightgallery-item', speed: 200, loop: false, hideBarsDelay: 2000 });
+  }
 });
