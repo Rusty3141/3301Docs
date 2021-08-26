b535841d612e561c2bf87e97179308a77ba4866f
Rusty3141 <89071215+Rusty3141@users.noreply.github.com>
2021-08-26 19:55:57 +0100

    Update generate_thumbnails.py

--- tools/generate_thumbnails.py
+++ tools/generate_thumbnails.py
@@ -16,7 +16,7 @@ def main():
             fullFilePath = os.path.join(root, fileName)
             if filetype.is_image(fullFilePath) and not(".thumbnail." in os.path.basename(fullFilePath)):
                 with Image.open(fullFilePath) as pageImage:
-                    if pageImage.size[0] > resizeWidthThreshold:
+                    if pageImage.size[0] >= resizeWidthThreshold:
                         pageImage.resize(
                             (round(pageImage.size[0] * resizeCoefficient), round(pageImage.size[1] * resizeCoefficient))).save("{0}{2}{1}".format(*os.path.splitext(fullFilePath), ".thumbnail"))
 
